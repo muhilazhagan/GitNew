d2d9ff0ca20f883b8a43dff77d1c8508b922d1ef
Muhilazhagan <muhilazhagan@gmail.com>
2022-10-14 20:24:07 +0400

    day 2 push

--- GitProject/src/main/java/com/example/demo/GitProjectApplication.java
+++ GitProject/src/main/java/com/example/demo/GitProjectApplication.java
@@ -10,4 +10,9 @@ public class GitProjectApplication {
 		SpringApplication.run(GitProjectApplication.class, args);
 	}
 
+	//docker build -t mydockerapp . // to build the application to image, . is required at the last to denote
+								    //	that the docker file in the current path
+	//docker run -itd -p 9091:9091 mydockerapp 
+	                                // to run the image
+	
 }
